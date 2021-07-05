51f43cd32ea5717db8eeebe4baa578b00d5e187b
shaquillo <chimishaquille@gmail.com>
2021-07-05 16:17:15 +0100

    modification of some field names

--- src/main/java/com/constelis/risk_management/entities/Note.java
+++ src/main/java/com/constelis/risk_management/entities/Note.java
@@ -16,23 +16,23 @@ public class Note {
 
     private Status status = Status.PENDING;
 
-    private Status document = Status.PENDING;
+    private Status document_status = Status.PENDING;
 
     private String document_link;
 
     private String offer;
 
-    private LocalDate created;
+    private LocalDate creation_date = LocalDate.now();
 
     private LocalDate offer_date;
 
     public Note(String note, Status status, Status document, String document_link, String offer, LocalDate created, LocalDate offer_date) {
         this.note = note;
         this.status = status;
-        this.document = document;
+        this.document_status = document;
         this.document_link = document_link;
         this.offer = offer;
-        this.created = created;
+        this.creation_date = created;
         this.offer_date = offer_date;
     }
 }
