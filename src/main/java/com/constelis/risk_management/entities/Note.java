79e7d5eafa9e0d03d7b7d4324d1b16941701f5c6
shaquillo <chimishaquille@gmail.com>
2021-07-13 10:50:48 +0100

    adding folder creation at startup

--- src/main/java/com/constelis/risk_management/entities/Note.java
+++ src/main/java/com/constelis/risk_management/entities/Note.java
@@ -3,10 +3,6 @@ package com.constelis.risk_management.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.web.multipart.MultipartFile;
-
-
-import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
@@ -21,23 +17,19 @@ public class Note {
 
     private Status document_status = Status.PENDING;
 
-    private MultipartFile file;
-
     private String filename;  //filename
 
     private String offer;
 
-    private LocalDate creation_date;
+    private String creation_date;
 
-    private LocalDate offer_date;
+    private String offer_date;
 
-    public Note(String note, Status status, Status document, MultipartFile file, String filename, String offer, LocalDate created, LocalDate offer_date) {
+    public Note(String note, Status status, Status document, String filename, String offer, String created, String offer_date) {
         this.note = note;
         this.status = status;
         this.document_status = document;
-        this.file = file;
         this.filename = filename;
-        this.filename = this.file.getOriginalFilename();
         this.offer = offer;
         this.creation_date = created;
         this.offer_date = offer_date;
